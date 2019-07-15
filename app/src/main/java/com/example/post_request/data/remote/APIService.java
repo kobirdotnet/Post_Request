e4a39845981904a4decb846c3059f43d71efefcd
Kobir <kobirdotnet@gmail.com>
2019-07-15 15:46:43 +0600

    Post_Request

---
+++ app/src/main/java/com/example/post_request/data/remote/APIService.java
@@ -0,0 +1,16 @@
+package com.example.post_request.data.remote;
+
+import com.example.post_request.data.model.Post;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface APIService {
+    @POST("/posts")
+    @FormUrlEncoded
+    Call<Post> savePost(@Field("title") String title,
+                        @Field("body") String body,
+                        @Field("userId") long userId);
+}
